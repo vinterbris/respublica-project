1827986ea46f27469b78c0382880729cd0746195
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-02-22 10:49:01 +0300

    finished init for product page

--- respublica_tests/pages/product_page.py
+++ respublica_tests/pages/product_page.py
@@ -4,14 +4,16 @@ from selene import browser
 class ProductPage:
 
     def __init__(self):
+        self._button_increase_amount = browser.element('[title="Увеличить количество товара"]')
+        self._button_buy = browser.element('.buy')
         self.checkout = browser.element('.buy.isActive')
 
     def add_to_cart(self):
-        browser.element('.buy').click()
+        self._button_buy.click()
 
     def go_to_cart(self):
-        browser.element('.buy.isActive').click()
+        self.checkout.click()
 
     def increase_amount_by(self, amount):
         for _ in range(amount):
-            browser.element('[title="Увеличить количество товара"]').click()
+            self._button_increase_amount.click()
